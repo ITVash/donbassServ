1df253c588e6d2c3a71e2637bcdc9058d9a5f300
Vash <vash_dn@mail.ru>
2020-01-12 14:39:34 +0300

    web-push

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,20 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
 import * as cors from 'cors';
+import * as dotenv from 'dotenv'
+import * as webpush from 'web-push'
 
 import { Auth, TimeTable } from './controllers';
 import { verifyToken } from './utils'
+
 const authCtrl = new Auth();
 const timeTableCtrl = new TimeTable();
 const app = express();
+dotenv.config()
 app.use(cors());
+
+webpush.setVapidDetails('mailto: vashdns@gmail.com', 'BDWa5QP_MgHnGivnoZ-rGGLWSUi1Ft0BK7eQWNQ0qxz4slVSa1r_6dCDWyCa9D-fgRk7lXqF5fKVnnu0XYtylW0', 'dD2ZbRnOnv3AisJBC0qBfUjzUI5-zgk4P3lLT_pI2BY')
+//webpush.setVapidDetails(process.env.WEB_PUSH_CONTACT, process.env.PUBLIC_VAPID_KEY, process.env.PRIVATE_VAPID_KEY)
 /**
  * Роуты авторизации пользователей
  */
@@ -27,5 +34,25 @@ app.get(`/timetable/:period`, verifyToken, timeTableCtrl.showPeriod);
 app.post('/timetable', verifyToken, timeTableCtrl.create);
 app.put('/timetable', verifyToken, timeTableCtrl.update);
 app.delete('/timetable', verifyToken, timeTableCtrl.delete);
+
+/**
+ * Роут Нотификации
+ */
+
+app.post('/notifications/subscribe', (req: express.Request, res: express.Response) => {
+  const subscription = req.body
+  console.log(subscription)
+  const payload = JSON.stringify({
+    title: 'Hello!',
+    body: 'It works.',
+  })
+
+  webpush.sendNotification(subscription, payload)
+    .then(result => console.log(result))
+    .catch(e => console.log(e.stack))
+
+  res.status(200).json({'success': true})
+})
+ 
 
 exports.api = functions.https.onRequest(app);
